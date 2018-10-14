acba655c2fb69c441cc6d5cff192ab221d0137ff
Victor Istratii <victor.istratii@faf.utm.md>
2018-10-15 00:27:52 +0300

    Added the factory classes.

---
+++ lab1/factory.h
@@ -0,0 +1,32 @@
+class Factory 
+{
+public:
+	virtual ForceStructure * createMunicipalStructure() = 0;
+	virtual ForceStructure * createRegionalStructure() = 0;
+}
+
+class SimplePoliceFactory : public Factory
+{
+public:
+	ForceStructure * createMunicipalStructure()
+	{
+		return new ParkPolice;
+	}
+	ForceStructure * createRegionalStructure()
+	{
+		return new HighwayPatrol;
+	}
+}
+
+class SpecialPoliceFactory : public Factory
+{
+public:
+	ForceStructure * createMunicipalStructure()
+	{
+		return new SWAT;
+	}
+	ForceStructure * createRegionalStructure()
+	{
+		return new StateTroopers;
+	}
+}
