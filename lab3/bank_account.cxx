5755249675432863dfba12cfc029f7aaf88c6d31
Victor Istratii <victor.istratii@faf.utm.md>
2018-12-02 00:03:04 +0200

    Added a proxy bank account data to implement the Poxy pattern.

--- lab3/bank_account.cxx
+++ lab3/bank_account.cxx
@@ -26,6 +26,30 @@ void AccountData::setCreditLimit(double c)
 	creditLimit = c;
 }
 
+ProxyAccountData::ProxyAccountData(double b, double c) : data(b, c)
+{
+
+}
+
+double ProxyAccountData::getBalance() const
+{
+	return data.getBalance();
+}
+
+double ProxyAccountData::getCreditLimit() const
+{
+	return data.getCreditLimit();
+}
+
+void ProxyAccountData::setBalance(double b)
+{
+	data.setBalance(b);
+}
+
+void ProxyAccountData::setCreditLimit(double c)
+{
+	data.setCreditLimit(c);
+}
 
 BankAccount::BankAccount(double b, double c) : data(b, c)
 {
--- lab3/bank_account.h
+++ lab3/bank_account.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 using namespace std;
 
 class AccountData
@@ -12,16 +14,62 @@ public:
 	AccountData(double b, double c);
 	double getBalance() const;
 	double getCreditLimit() const;
-	void setBalance();
-	void setCreditLimit();
+	void setBalance(double b);
+	void setCreditLimit(double c);
 };
 
-class BankAccount
+class ProxyAccountData
 {
 private:
 	AccountData data;
 
 public:
-	BankAccount();
+	ProxyAccountData(double b, double c);
+	double getBalance() const;
+	double getCreditLimit() const;
+	void setBalance(double b);
+	void setCreditLimit(double c);
+};
+
+class BankAccount
+{
+protected:
+	ProxyAccountData data;
+
+public:
+	BankAccount(double b, double c);
+	virtual ~BankAccount(){};
 	void report();
+};
+
+class RegularBankAccount : public BankAccount
+{
+public:
+	RegularBankAccount(double b, double c) : BankAccount::BankAccount(b, c)
+	{
+
+	}
+	void report()
+	{
+		cout << "This is a pleb regular account. It has a small credit limit. Here are the stats:" << endl;
+		cout << "Balance: " << data.getBalance() << endl;
+		cout << "Credit limit: " << data.getCreditLimit() << endl; 
+	}
+};
+
+class PremiumBankAccount : public BankAccount
+{
+public:
+	PremiumBankAccount(double b, double c) : BankAccount::BankAccount(b, c)
+	{
+
+	}
+	void report()
+	{	
+		cout << "*****" << endl;
+		cout << "This is a $WAG PrEmIuM BaNk AcCoUnT. However the only difference from a regular one is the increased credit limit." << endl;
+		cout << "Balance: " << data.getBalance() << endl;
+		cout << "Credit limit: " << data.getCreditLimit() << endl; 
+		cout << "*****" << endl;
+	}
 };
