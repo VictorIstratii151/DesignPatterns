e7eb4418146cff104246d0a639531e7402c79274
Victor Istratii <victor.istratii@faf.utm.md>
2018-12-01 21:01:24 +0200

    Added c class for storring account data to ensure private class data pattern.

--- lab3/bank_account.h
+++ lab3/bank_account.h
@@ -2,13 +2,26 @@
 
 using namespace std;
 
+class AccountData
+{
+private:
+	double balance;
+	double creditLimit;
+
+public:
+	AccountData(double b, double c);
+	double getBalance() const;
+	double getCreditLimit() const;
+	void setBalance();
+	void setCreditLimit();
+};
+
 class BankAccount
 {
 private:
-	float balance;
-	float creditLimit;
+	AccountData data;
 
 public:
 	BankAccount();
-
+	void report();
 };
